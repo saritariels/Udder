97c1448dc962672391bbe9c8747f8f50a7c2792d
Sarit Ariel <saritariel21@gmail.com>
2023-02-01 11:48:12 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Sarit Ariel on 01.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
